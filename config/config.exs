d30dddf590910bbcf5926a976f05d78dd236ea77
r.nikiforov <r.nikiforov@tinkoff.ru>
2021-10-31 15:46:56 +0300

    update dependency set floki parser to fast_html

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,3 @@
 use Mix.Config
 
-config :floki, :html_parser, Floki.HTMLParser.Html5ever
+config :floki, :html_parser, Floki.HTMLParser.FastHtml
--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule MeeseeksFlokiBench.Mixfile do
   def project do
     [
       app: :meeseeks_floki_bench,
-      version: "0.1.0",
-      elixir: "~> 1.4",
+      version: "0.1.1",
+      elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -19,9 +19,11 @@ defmodule MeeseeksFlokiBench.Mixfile do
   defp deps do
     [
       {:benchee, "~> 1.0.1"},
-      {:floki, "~> 0.26.0"},
-      {:html5ever, "~> 0.8.0"},
-      {:meeseeks, "~> 0.15.0"}
+      {:floki, "~> 0.32.0"},
+      {:html5ever, "~> 0.9.0"},
+      {:meeseeks_html5ever, "~> 0.13.1"},
+      {:meeseeks, "~> 0.16.1"},
+      {:fast_html, "~> 2.0.4"}
     ]
   end
 end
